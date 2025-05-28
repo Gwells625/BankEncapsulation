948c6ca420daec9d194bdcf1d676e9ea7ca85b29
Geoffrey Wells <Gwells625@gmail.com>
2025-05-28 16:38:55 -0400

    Completed Bank Encapsulation exercise 1.

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,27 @@
+namespace BankEncapsulation;
+
+public class BankAccount
+{
+    public BankAccount()
+    {
+    }
+    private double balance = 0;
+    
+    //Define a method named Deposit that will accept a double
+    //and store that value in the balance field
+
+    public void Deposit(double amount)//Encapsulation
+    {
+        balance = amount;
+    }
+    
+    //Define a method named GetBalance that 
+    //will return the amount stored in the balance field
+
+    public string GetBalance()
+    {
+        return $"${balance}";
+    }
+
+
+}
